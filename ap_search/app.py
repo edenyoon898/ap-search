d6d45f46f4dcaf251bbe0d253de2e4acd78d3a3a
edenyoon <edenyoon898@gmail.com>
2021-04-18 22:06:59 +0900

    RedisCache로 API 응답 캐싱

--- ap_search/app.py
+++ ap_search/app.py
@@ -1,18 +1,22 @@
-import redis
 from flask import Flask, request
+from flask_caching import Cache
 
 from ap_search.query_es.product import ProductQueryClient
 
 app = Flask(__name__)
-cache = redis.Redis(host="ap-redis", port=6379)
+cache = Cache(app, config={
+    'CACHE_TYPE': 'RedisCache', 'CACHE_REDIS_URL': 'redis://ap-redis:6379/0'
+})
 
 
 @app.route("/")
+@cache.cached(timeout=300)
 def hello():
     return "Hello World!"
 
 
 @app.route("/search/v1/product")
+@cache.cached(timeout=30, query_string=True)
 def search_v1_product():
     keyword = request.args.get("keyword", type=str)
     offset = request.args.get("offset", 0, type=int)
@@ -27,6 +31,7 @@ def search_v1_product():
 
 
 @app.route("/search/v1/product/<int:product_no>")
+@cache.cached(timeout=30, query_string=True)
 def search_v1_product_get(product_no: int):
     result = ProductQueryClient().get(product_no)
     return result
